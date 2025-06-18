768677b1f242461371503d109ed08570f391791c
kais_dev <kaishasson9@gmail.com>
2025-06-19 01:16:32 +0300

    update migrations

---
+++ app/Http/Controllers/FavorateBooksController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Updatefavorite_BooksRequest;
+use App\Models\favorate_books;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class FavorateBooksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $favoriteBooks = Auth::user()->favoriteBooks()->with('favoriteBooks')->get();
+
+        return response()->json([
+            'data' => $favoriteBooks,
+            'status' => 200,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'book_id' => 'required|exists:books,id',
+        ]);
+
+        $favoriteBooks = Auth::user()->favoriteBooks()->create($validated);
+
+        return response()->json([
+            'data' => $favoriteBooks,
+            'status' => 201,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $favoriteBooks = Auth::user()->favoriteBooks()->with('favoriteBooks')->findOrFail($id);
+
+        return response()->json([
+            'data' => $favoriteBooks,
+            'status' => 200,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Updatefavorite_BooksRequest $request, favorate_books $favorite_Books)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(favorate_books $favorite_Books)
+    {
+        //
+    }
+}
---
+++ app/Models/favorate_books.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class favorate_books extends Model
+{
+    /** @use HasFactory<\Database\Factories\FavorateBooksFactory> */
+    use HasFactory;
+    protected $fillable = ['book_id','user_id'];
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function book(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Book::class);
+    }
+}
