aa85dd1270c7a89c56b0bda3c54e48a3ea0a7eca
kais_dev <kaishasson9@gmail.com>
2025-06-19 21:54:57 +0300

    update migrations 3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,6 @@ namespace App\Providers;
 use Laravel\Passport\Passport;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\File;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -18,7 +17,6 @@ class AppServiceProvider extends ServiceProvider
     }
 
     /**
-     *
      * Bootstrap any application services.
      */
     public function boot(): void
@@ -27,18 +25,5 @@ class AppServiceProvider extends ServiceProvider
 
         Passport::tokensExpireIn(now()->addDays(15));
         Passport::refreshTokensExpireIn(now()->addDays(30));
-        if (app()->environment('production')) {
-            $privateKey = storage_path('oauth-private.key');
-            $publicKey = storage_path('oauth-public.key');
-
-            if (env('PASSPORT_PRIVATE_KEY_B64') && env('PASSPORT_PUBLIC_KEY_B64')) {
-                File::put($privateKey, base64_decode(env('PASSPORT_PRIVATE_KEY_B64')));
-                File::put($publicKey, base64_decode(env('PASSPORT_PUBLIC_KEY_B64')));
-
-                // ✅ Set secure permissions
-                @chmod($privateKey, 0600);
-                @chmod($publicKey, 0600);
-            }
-        }
     }
 }
