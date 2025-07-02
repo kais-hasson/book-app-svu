815898ffa439d28c36aa50718b9be06358a97b82
kais_dev <kaishasson9@gmail.com>
2025-07-02 20:54:09 +0300

    >>

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,7 @@
 use App\Models\Book;
 use App\Models\Category_book;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Log;
 
-Route::post('/livewire/upload-file', function () {
-    Log::info('Upload File Cookies', request()->cookies->all());
-    abort(401, 'Debugging 401');
-});
 Route::fallback(function () {
     return redirect('/admin'); // or return view('home');
 });
